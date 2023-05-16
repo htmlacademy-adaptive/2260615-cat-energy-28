b70fd65ad1fa31561c576a0f455327eb641a6a8f
IlyaPushkarskiy <p_ilya_007@mail.ru>
2023-05-16 13:27:18 +0300

    начал автоматизацию

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,70 @@ import gulp from 'gulp';
 import plumber from 'gulp-plumber';
 import less from 'gulp-less';
 import postcss from 'gulp-postcss';
+import csso from 'postcss-csso';
+import rename from 'gulp-rename';
 import autoprefixer from 'autoprefixer';
 import browser from 'browser-sync';
-
+import terser from 'gulp-terser';
+import squoosh from 'gulp-libsquoosh';
+import htmlmin from 'gulp-htmlmin';
 // Styles
 
 export const styles = () => {
-  return gulp.src('source/less/style.less', { sourcemaps: true })
-    .pipe(plumber())
-    .pipe(less())
-    .pipe(postcss([
-      autoprefixer()
+  return gulp.src('source/less/style.less', { sourcemaps: true }) // находит style.less
+    .pipe(plumber()) //обработка ошибок
+    .pipe(less()) //style.less > style.css
+    .pipe(postcss([ //style.css
+      autoprefixer(),//style.csss > style.css[prefix]
+      csso() //style.csss > style.css[prefix] > style.css[prefix].min
     ]))
-    .pipe(gulp.dest('source/css', { sourcemaps: '.' }))
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('build/css', { sourcemaps: '.' })) // source/css
     .pipe(browser.stream());
+}
+
+//HTML
+export const html = () => {
+  return gulp.src('source/*.html')
+  .pipe(htmlmin({ collapseWhitespace: true }))
+  .pipe(gulp.dest('build'));
+}
+
+//scripts
+export const scripts = () => {
+  return gulp.src('source/js/*.js')
+  .pipe(terser())
+  .pipe(gulp.dest('build/js'));
+}
+
+//images
+export const optimizeImages = () => {
+  return gulp.src('source/img/**/*.{jpg,png}')
+  .pipe(squoosh())
+  .pipe(gulp.dest('build/img'));
+}
+
+const copyImages = () => {
+  return gulp.src('source/img/**/*.{jpg,png}')
+  .pipe(gulp.dest('build/img'));
+}
+
+//WebP
+export const createWebp= () => {
+  return gulp.src('source/img/**/*.{jpg,png}')
+  .pipe(squoosh({
+    webp: {}
+  }))
+  .pipe(gulp.dest('build/img'));
+}
+
+//svg
+export const svg= () => {
+  return gulp.src('source/img/**/*.svg')
+  .pipe(squoosh({
+    webp: {}
+  }))
+  .pipe(gulp.dest('build/img'));
 }
 
 // Server
@@ -23,7 +73,7 @@ export const styles = () => {
 const server = (done) => {
   browser.init({
     server: {
-      baseDir: 'source'
+      baseDir: 'build'
     },
     cors: true,
     notify: false,
@@ -41,5 +91,5 @@ const watcher = () => {
 
 
 export default gulp.series(
-  styles, server, watcher
+  html, scripts, optimizeImages,  createWebp, styles, server, watcher
 );
